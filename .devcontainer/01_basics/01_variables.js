5fbd295da3a4a148746bb9ad753da97441df67a1
imranappy <96012095+imranappy@users.noreply.github.com>
2025-01-15 19:01:31 +0000

    Varibles in JS

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "imran@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2 //
+
+accountEmail = "imran1@gmail.com"
+accountPassword = "123123"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
