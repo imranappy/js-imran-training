fd023dd1c267e2f9eb7f5245a679fd9e865517bf
imranappy <96012095+imranappy@users.noreply.github.com>
2025-01-16 18:14:45 +0000

    Datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log( 3
+    +3)            // code should be readable
+
+let name = "Imran"
+let age = 36
+let isLoggedIn = false
+
+console.log(typeof undefined);
