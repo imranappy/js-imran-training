4ba7936f1b2aa140decdbbf855fac773650e6e9d
imranappy <96012095+imranappy@users.noreply.github.com>
2025-01-17 15:10:43 +0000

    comparisons

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -23,5 +23,18 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 44
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//--------------- Operations ---------------------
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " imran"
+
+let str3 = str1 + str2
+console.log(str3);
