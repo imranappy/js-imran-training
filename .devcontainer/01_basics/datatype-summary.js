08617d257b27c52a07e2ca47446bf77fef9f8320
imranappy <96012095+imranappy@users.noreply.github.com>
2025-01-17 15:46:54 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatype-summary.js
@@ -0,0 +1,30 @@
+// Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 46364364364364836
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+
+const heroes = ["srk", "sak", "krishh"];
+let myObj = {
+    name: " imran ",
+    age: 25
+}
+
+const myFunction = function() {
+   console.log("Hello world");
+    
+}
