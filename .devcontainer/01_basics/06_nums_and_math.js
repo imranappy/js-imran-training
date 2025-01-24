b4a1ada34aa1a7c8397cdcf06dbd9cc85054de74
imranappy <96012095+imranappy@users.noreply.github.com>
2025-01-24 07:10:34 +0000

    nums and maths

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString(`en-IN`));
+
+// -----------------------------------------------------------Math--------------------------------------
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(7.8));
+//console.log(Math.ceil(5.8));
+//console.log(Math.floor(5.8));
+//console.log(Math.min(0,1,2,3,55));
+//console.log(Math.max(99,100,98,101));
+
+//console.log(Math.random());
+//console.log(Math.random()*10 + 1);
+//console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random(max - min)*10) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
