2fc96ce2cc6ae7db87b104caddd591a4c52b30ac
imranappy <96012095+imranappy@users.noreply.github.com>
2025-01-24 18:38:00 +0000

    dates in javascript

---
+++ .devcontainer/01_basics/07_datesinjs.js
@@ -0,0 +1,29 @@
+
+let mydate = new Date()
+//console.log(mydate);
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());
+//console.log(mydate.toLocaleDateString());
+//console.log(mydate.toLocaleTimeString());
+//console.log(typeof mydate);
+
+let myCreateDate = new Date(2025, 0, 23, 26, 5)
+//console.log(myCreateDate);
+//console.log(myCreateDate.toDateString());
+//console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString
+
+
+
+
